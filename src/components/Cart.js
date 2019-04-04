98f2f9322d3fa1f078fc4490cec00da069bce10b
Rafael Garcia <rafgarciaa@gmail.com>
2019-04-04 02:04:33 -0400

    Implement cart modal using CSS (desktop only)

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -31,9 +31,24 @@ const Cart  = ({ products, total, onCheckoutClicked }) => {
   */}
 
   return (
-    <div className="cart">
-      <i className="fas fa-shopping-cart"></i>
-      <h3>Your Cart {cartQty > 0 ? `[${cartQty}]` : `is empty`}</h3>
+    <div>
+      <a href="#cart-modal" className="cart">
+        <i className="fas fa-shopping-cart"></i>
+        <h3>Your cart {cartQty > 0 ? `[${cartQty}]` : `is empty`}</h3>
+      </a>
+
+      <div id="cart-modal" className="cart-modal">
+        <div className="cart-modal__content">
+          <a href="#root" className="cart-modal__close-button">
+            <span className="cart-modal__close-icon cart-modal__close-icon-1">&nbsp;</span>
+            <span className="cart-modal__close-icon cart-modal__close-icon-2">&nbsp;</span>
+          </a>
+
+          <h3 className="cart-modal__content-heading">Your Cart</h3>
+          <img className="cart-modal__content-img" src="./img/cart.png"></img>
+          <span className="cart-modal__content-message">Please add some products to your cart.</span>
+        </div>
+      </div>
     </div>
   )
 }
