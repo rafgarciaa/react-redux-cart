05f8c0f3557b9f111de9912adb4ca68a023a3fa5
Rafael Garcia <rafgarciaa@gmail.com>
2019-04-10 08:40:25 -0400

    Updates

---
+++ precache-manifest.f89f7e4d56e59debab09115ecfd0ead0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "25bcadef0f843b9c1bd6",
+    "url": "/react-redux-cart/static/css/main.2c17ae24.chunk.css"
+  },
+  {
+    "revision": "25bcadef0f843b9c1bd6",
+    "url": "/react-redux-cart/static/js/main.25bcadef.chunk.js"
+  },
+  {
+    "revision": "f1045cfc66a571a1a195",
+    "url": "/react-redux-cart/static/js/runtime~main.f1045cfc.js"
+  },
+  {
+    "revision": "b1a95be0eb4b75b491f6",
+    "url": "/react-redux-cart/static/js/2.b1a95be0.chunk.js"
+  },
+  {
+    "revision": "73980d1fbb7824eab74e7d1331890bf1",
+    "url": "/react-redux-cart/static/media/Chivo-Bold.73980d1f.ttf"
+  },
+  {
+    "revision": "835a07fa00861fb42031a825296117f9",
+    "url": "/react-redux-cart/static/media/Chivo-Light.835a07fa.ttf"
+  },
+  {
+    "revision": "07ad6513875bfaa020596c8d92085765",
+    "url": "/react-redux-cart/static/media/Chivo-Regular.07ad6513.ttf"
+  },
+  {
+    "revision": "5754a536be2476d2c8eaddb27fa300d2",
+    "url": "/react-redux-cart/static/media/HelveticaNeue.5754a536.ttf"
+  },
+  {
+    "revision": "de9ad2fecee4583f779302d953b51252",
+    "url": "/react-redux-cart/index.html"
+  }
+];
